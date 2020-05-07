6f60bdf4e00fd06dca44a8c9a118ba982d33bfa4
Serhiy Zahoriya <serhiy.int@gmail.com>
2020-05-07 03:51:52 +0300

    0.1: no shell script, default executable, readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+0. Make sure you have Python 3.
+1. Edit `openbydomain.csv`.
+2. Make `openbydomain.py` executable.
+3. Make `openbydomain.py` your default browser application.
--- openbydomain.py
+++ openbydomain.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
 import csv
+import os
+import subprocess
+import sys
 
 from urllib.parse import urlparse
 
@@ -9,14 +11,18 @@ from urllib.parse import urlparse
 if len(sys.argv) < 2:
     exit()
 
+os.chdir(os.path.dirname(sys.argv[0]))
+
 parsed_url = urlparse(sys.argv[1])
 domain = parsed_url.netloc
-
-if not domain:
-    exit()
 
 with open('openbydomain.csv') as csvfile:
     reader = csv.reader(csvfile)
     dispatch_dict = {row[0]: row[1] for row in reader}
 
-print(dispatch_dict[domain])
+if 'default' not in dispatch_dict:
+    print('No default executable in config', file=sys.stderr)
+    exit()
+
+executable = dispatch_dict.get(domain, dispatch_dict['default'])
+subprocess.Popen([executable, sys.argv[1]])
